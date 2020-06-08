89e1f8c5233f44c569f5904877333d8b2b4622dc
itland development <development@itlandgroup.com>
2020-06-08 20:38:29 +0200

    Updates

---
+++ precache-manifest.a415982c85c4caa84b75127635edcd30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d6b3a442d335a6a971b",
+    "url": "/static/js/main.3d6b3a44.chunk.js"
+  },
+  {
+    "revision": "1edeba0c29f661d10ed7",
+    "url": "/static/js/1.1edeba0c.chunk.js"
+  },
+  {
+    "revision": "3d6b3a442d335a6a971b",
+    "url": "/static/css/main.83566f4f.chunk.css"
+  },
+  {
+    "revision": "1edeba0c29f661d10ed7",
+    "url": "/static/css/1.ad1808cc.chunk.css"
+  },
+  {
+    "revision": "373031277ec0b1a7869da4dcdd37262f",
+    "url": "/index.html"
+  }
+];
