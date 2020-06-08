6932b960a3dcf668e561fb35eb0f4145f19511e7
itland development <development@itlandgroup.com>
2020-06-08 20:40:14 +0200

    Updates

---
+++ precache-manifest.227538028be954053c95b19f10c99f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dc99e31d907442f3bb9",
+    "url": "/itland-store/static/js/runtime~main.3dc99e31.js"
+  },
+  {
+    "revision": "d66a509864ede20184c2",
+    "url": "/itland-store/static/js/main.d66a5098.chunk.js"
+  },
+  {
+    "revision": "a18e4b6a2f9d3fc630fd",
+    "url": "/itland-store/static/js/1.a18e4b6a.chunk.js"
+  },
+  {
+    "revision": "d66a509864ede20184c2",
+    "url": "/itland-store/static/css/main.83566f4f.chunk.css"
+  },
+  {
+    "revision": "a18e4b6a2f9d3fc630fd",
+    "url": "/itland-store/static/css/1.ad1808cc.chunk.css"
+  },
+  {
+    "revision": "642c2bb672d270d0d856b4c3aa22f44a",
+    "url": "/itland-store/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a415982c85c4caa84b75127635edcd30.js"
+  "/itland-store/precache-manifest.227538028be954053c95b19f10c99f7c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/itland-store/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
